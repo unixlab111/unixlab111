22fe70584842e3a62f0f44e95b64995d557783e3
unixlab111 <mohakmugulurmutt2@gmail.com>
2024-08-29 16:50:13 +0530

    Create SPEFfirstss.sh

---
+++ SPEFfirstss.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "1.size\n2.permission\n3.existence of link\n4.filename"
+
+echo "enter the file:"
+read file
+
+if [ ! -f $file ]
+then
+
+echo "file does not exist"
+exit
+
+fi
+
+echo "enter the choice:"
+read choice
+
+case $choice in
+
+1 ) ls -l $file | cut -d ' ' -f  5 ;;
+2 ) ls -l $file | cut -d ' ' -f  1 ;;
+3 ) ls -l $file | cut -d ' ' -f  2 ;;
+4 )  ls -l $file | cut -d ' ' -f  10 ;;
+
+* ) echo "invalid choice....";;
+
+esac
